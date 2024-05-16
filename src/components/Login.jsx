5c51ed37169029a88e74721d69f23c9f1c1859d6
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-05-16 21:19:31 +0530

    re-updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,6 @@ const Login = () => {
 
   const handleSubmit = () => {
     if (user.username === "qaifi" && user.password === "qaifi") {
-      // Set a flag in local storage indicating successful login
       localStorage.setItem("isLoggedIn", "true");
       alert("Login successful");
       navigate("/orders");
@@ -20,37 +19,9 @@ const Login = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "90vh",
-        maxWidth: "500px",
-        margin: "0 auto",
-        width: "100%",
-      }}
-    >
-      <div
-        style={{
-          padding: "40px",
-          display: "flex",
-          flexDirection: "column",
-          backgroundColor: "#fff",
-          gap: "35px",
-          width: "100%", // Set width to 100% to fill the parent container
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "32px",
-            letterSpacing: "1px",
-            fontWeight: "500",
-          }}
-        >
-          Sign In
-        </h1>
+    <div className="login-container">
+      <div className="login-subcontainer">
+        <h1>Sign In</h1>
         <input
           type="text"
           name=""
@@ -63,12 +34,6 @@ const Login = () => {
           }
           id=""
           placeholder="Enter Username"
-          style={{
-            width: "100%",
-            border: "none",
-            borderBottom: "2px solid #8c8c8c",
-            outline: "none",
-          }}
         />
         <input
           type="password"
@@ -82,26 +47,8 @@ const Login = () => {
               password: e.target.value,
             })
           }
-          style={{
-            width: "100%",
-            border: "none",
-            borderBottom: "2px solid #8c8c8c",
-            outline: "none",
-          }}
         />
-        <button
-          style={{
-            padding: "15px",
-            borderRadius: "25px",
-            border: "none",
-            backgroundColor: "#20b983",
-            color: "#fff",
-            cursor: "pointer",
-          }}
-          onClick={handleSubmit}
-        >
-          Log In
-        </button>
+        <button onClick={handleSubmit}>Log In</button>
       </div>
     </div>
   );
